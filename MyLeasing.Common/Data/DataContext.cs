ccf802526fbdefeaa676466bb4e4ce448afebd85
Nuno Gonçalo Silva <ngoncalorsilva@gmail.com>
2022-07-18 05:11:07 +0100

    add-migration InitDb update-database

---
+++ MyLeasing.Common/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLeasing.Web.Data.Ententies;
+
+namespace MyLeasing.Common
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Owner> Owners { get; set; }
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ MyLeasing.Common/Data/Ententies/Owner.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Ententies
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        public int Document { get; set; }
+
+        [Display(Name = "Owener Name")]
+        public string OwnerName { get; set; }
+
+        [Display(Name = "Fixed Phone")]
+        public int FixedPhone { get; set; }
+
+        public string Adress { get; set; }
+    }
+}
